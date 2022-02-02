88a70c2ea3a5de12cdb81a765471f1f9a2eca383
Shivanshu17 <sharma.shivanshu98@gmail.com>
2022-02-02 20:00:47 +0530

    Adding new project with practice notebook on tf.data entry

---
+++ 30_Tabular_Practice/tabular_practice/src/models/train_model.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def train_model(type, version):
+    '''
+    Train & save the model (with the version provided)
+    '''
